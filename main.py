a8a75b1b111c41114cfec75433eae6aa6c4a2b9f
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-04-11 16:00:59 -0400

    slowly but surely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import urllib
+from urllib.parse import urlparse
 import json
 import os
 
@@ -7,53 +7,109 @@ import os
 GROUP_PATH = './group_data/url_groups.json'
 
 ## URL key numbers are irrelevant; only for key differentiation; URLs are opened sequentially based on dict position
-def new_url_group(group:dict={'name':'group','url1':'url','url2':'url'}):
-    url_keys = [key for key in group.keys() if key in 'url'.casefold()]
-    if not group.has_key('name') and :
-        print("group dictionary must contain group name key")
-    elif url_keys.isEmpty():
-        print("no URL keys specified")
-    elif not 'name' in group.keys() and not 'url' in group.keys():
-        print("only 'name' and 'url' keys accepted only")
+## ensure your urls contain scheme (http/https) and netloc(domain)
+def new_url_group(group={'name': {'url1':'http://www.google.com','url2':'https://www.amazon.com'}}) -> None:
+    url_tuples = [()]
+    for url_dict in group.values():
+        if isinstance(url_dict, dict):
+            url_tuples.extend([(key, value) for key, value in url_dict.items()])
+            key, value = url_tuples[-1]
+            if not isinstance(value, str):
+                print("contains non-string value"); return
+            elif not all([urlparse(value).scheme, urlparse(value).netloc]):
+                print("contains invalid URL"); return
+        else:
+            print("contains non-dict value"); return
+
+    if url_tuples == [()]:
+        print("no URL pairs specified"); return
     
-    mode = 'w' if os.GROUP_PATH.isfile(GROUP_PATH) else 'a'
-    with open(GROUP_PATH, mode) as output:
-        json.dump(group, output)
-        
-def get_url_group(group_name:str="") -> dictionary:
+    mode = 'a' if os.path.isfile(GROUP_PATH) else 'w'
+    with open(GROUP_PATH, mode) as output: 
+        if mode == 'w':
+            groups = {'groups':group}
+            json.dump(groups, output, indent=4)
+        else:
+            json.dump(group, output, indent=4)
+
+def get_url_group_names() -> list:
     try:
-        with open(GROUP_PATH, 'r') as input:
-            groups = json.load(input)
-    except IOError as e:
-        print e
-        print("IOError: Unable to open url_groups.json. Terminating execution.")
-        exit(1)
+        with open(GROUP_PATH, 'r') as file:
+            groups = json.load(file)
+    except IOError:
+        print("IOError: Unable to open url_groups.json. Terminating execution."); return
+
+    return list(groups['groups'].keys())
+
+
+def get_url_group(group_name:str="") -> dict:
+    try:
+        with open(GROUP_PATH, 'r') as file:
+            groups = json.load(file)
+    except IOError:
+        print("IOError: Unable to open url_groups.json. Terminating execution."); return
         
-    if group_name in groups['name'].values():
-        print groups
-    else
-        print("group name not found")
-        
+    return groups['groups'][group_name]
         
 def edit_url_group(group_name:str=''):
     group = get_url_group(group_name)
     
     with open(GROUP_PATH, 'a') as output:
-        
+        pass
     
 
 def remove_url_group(group_name:str=''):
     group = get_url_group(group_name)
     
-    
+def sort_file_by_name(path:str=''):
+    pass
 
 def open_url_group(group_name:str=''):
-    
+    group = get_url_group(group_name)
+    pass
 
-def main():
-    
-    
-    
-    
-if __init__ == main:
-    main()
+ACTIONS = ['open group', 'list groups', 'new group', 'edit group', 'remove group']
+while True:
+    print ("Avaiable commands: ")
+    for action in ACTIONS: print(action)
+    while True:
+        action = input("Enter action: ")
+        if action in ACTIONS: break
+        else: print("invalid action")
+    if action == 'open group':
+        group_name = input("Enter group name: ")
+        open_url_group(group_name)
+    if action == 'list groups':
+        for group in 
+    if action == 'new group':
+        group={}
+        groups = {{}}
+        urls = [()]
+        while True:
+            while True:
+                group_name = input("Enter group name: ")
+                if group_name == '': print("invalid group name")
+                else: break
+            while True:
+                url = input("Enter URL name: ")
+                if url == '': print("invalid url name")
+                else: 
+                    while True:
+                        url = (url, input("Enter URL: "))
+                        if url[1] == '': print("invalid url")
+                        else:
+                            urls.append(url)
+                            break
+                if input("Add another URL? (y/n) ").lower() == 'n': break
+                
+            for url in urls:
+                url_name, link = url
+                group[group_name] = {url_name: link}
+            new_url_group(group)
+            if input("Add another group? (y/n) ").lower() == 'n': break
+    if action == 'edit group':
+        group_name = input("Enter group name: ")
+        edit_url_group(group_name)
+    if action == 'remove group':
+        group_name = input("Enter group name: ")
+        remove_url_group(group_name)
