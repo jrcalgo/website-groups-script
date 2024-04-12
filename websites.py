20e7c22b645c4f337a828710f16bd11710973723
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-04-12 12:31:58 -0400

    please work

--- websites.py
+++ websites.py
@@ -50,6 +50,7 @@ def get_url_group_names() -> list:
     return list(groups['groups'].keys())
 
 def get_url_group(group_name:str="") -> dict:
+    if group_name in get_url_group_names(): print("invalid group name"); return
     try:
         with open(GROUP_PATH, 'r') as file:
             groups = json.load(file)
@@ -57,23 +58,30 @@ def get_url_group(group_name:str="") -> dict:
         print("IOError: Unable to open url_groups.json. Terminating execution."); return
         
     return groups['groups'][group_name]
-        
-def edit_url_group(group_name:str='', new_group_name:str='', new_urls:dict={}):
+       
+## when editing urls, new_urls will replace all existing urls in said group if append is False
+## warning: append being True will still overwrite existing urls if key is the same as existing url
+def edit_url_group(group_name:str='', new_group_name:str='', new_urls:dict={}, append_urls:bool=False):
     group = get_url_group(group_name)
     with open(GROUP_PATH, 'r+') as output:
         if group_name in get_url_group_names():
             groups = json.load(output)
             if new_group_name != '':
-                group = groups['groups'].pop(group_name)
+                groups['groups'].pop(group_name)
                 groups['groups'][new_group_name] = group
+                group = groups['groups'][new_group_name]
             if new_urls != {}:
-                pass
+                for new_url in new_urls.items():
+                    if append_urls:
+                        group[new_url[0]] = new_url[1]
+                    else:
+                        group.update({new_url[0]: new_url[1]})
             output.seek(0)
             groups['groups'].update(group)
             output.seek(0)
             json.dump(groups, output, indent=4)
         else:
-            print("group name does not exist"); return
+            print("**group name does not exist**"); return
 
 def remove_url_group(group_name:str=''):
     group = get_url_group(group_name)
@@ -85,7 +93,7 @@ def remove_url_group(group_name:str=''):
             output.truncate()
             json.dump(groups, output, indent=4)
         else:
-            print("group name does not exist"); return
+            print("**group name does not exist**"); return
 
 def sort_file_by_name():
     with open(GROUP_PATH, 'r+') as file:
@@ -97,17 +105,20 @@ def sort_file_by_name():
         json.dump(groups, file, indent=4)
 
 def open_url_group(group_name:str=''):
-    group = get_url_group(group_name)
-    if group:
-        first_url = True
-        for url in group.values():
-            if first_url:
-                webbrowser.open(url, new=1)
-                first_url = False
-            else:
-                webbrowser.open_new_tab(url)
-    else:
-        return
+    try:
+        group = get_url_group(group_name)
+        if group:
+            first_url = True
+            for url in group.values():
+                if first_url:
+                    webbrowser.open(url, new=1)
+                    first_url = False
+                else:
+                    webbrowser.open_new_tab(url)
+        else:
+            return
+    except:
+        print("**error opening group**"); return
 
 COMMANDS = ['open', 'list', 'new', 'edit', 'remove']
 while True:
@@ -143,29 +154,50 @@ while True:
                         url = (url, input("Enter URL: "))
                         if not all([urlparse(url[1]).scheme, urlparse(url[1]).netloc]): print("invalid url")
                         else: urls.append(url); break
-                if input("Add another URL? (y/n): ").lower() == 'n': break
+                if input("Add another URL? (y/n): ").casefold() == 'n': break
             for url in urls:
                 url_name, link = url
                 group[group_name] = {url_name: link}
             new_url_group(group)
-            if input("Add another group? (y/n): ").lower() == 'n': break
+            if input("Add another group? (y/n): ").casefold() == 'n': break
         sort_file_by_name()
     elif command == COMMANDS[3]:
+        group_name = ''
+        new_group_name = ''
+        new_urls = {}
+        append_urls = False
         while True:
             group_name = input("Enter group name: ")
             if group_name == '': print("invalid group name")
+            elif group_name in get_url_group_names(): print("group name already exists")
             else: break
         while True:
             new_group_name = input("New group name? (y/n): ")
-            if new_group_name.lower() == 'y':
-                new_group_name = input("Enter new group name: ")
-                if new_group_name == '': print("invalid group name")
-                elif new_group_name in get_url_group_names(): print("group name already exists")
-                else: break
+            if new_group_name.casefold() == 'y':
+                while True:
+                    new_group_name = input("Enter new group name: ")
+                    if new_group_name == '': print("invalid group name")
+                    elif new_group_name in get_url_group_names(): print("group name already exists")
+                    else: break
             else: break
         while True:
-            pass
-        edit_url_group(group_name)
+            new_urls = input("Enter new URLs? (y/n): ")
+            if new_urls.casefold() == 'y':
+                new_url = input("Enter URL name: ")
+                if new_url == '': print("invalid url name")
+                else:
+                    while True:
+                        while True:
+                            new_url = (new_url, input("Enter URL: "))
+                            if not all([urlparse(new_url[1]).scheme, urlparse(new_url[1]).netloc]): print("invalid url")
+                            else: new_urls[new_url[0]] = new_url[1]
+                        if input("Add another URL? (y/n): ").casefold() == 'n': break
+                    while True:
+                        append_urls = input("Append to existing group? (y/n): ")
+                        if append_urls.casefold() == 'y': append_urls = True; break
+                        elif append_urls.casefold() == 'n': append_urls = False; break
+                        else: print("invalid response")
+        edit_url_group(group_name, new_group_name, new_urls, append_urls)
     elif command == COMMANDS[4]:
         group_name = input("Enter group name: ")
         remove_url_group(group_name)
