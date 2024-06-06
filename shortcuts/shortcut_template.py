95af1c8e5e4aacfa0d2b561346cfc35c2727d480
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-06-06 11:40:16 -0400

    OUR Work tabs + shortcut directory created

--- shortcuts/shortcut_template.py
+++ shortcuts/shortcut_template.py
@@ -4,7 +4,7 @@ from websites import open_url_group
 ## when you make your own shortcut script:
 ## ensure you refactor this method call to your group name... unless 'your_group_name_here' is your group name, in which
 ## case you can just leave it as it is. 
-open_url_group('your_group_name_here')
+open_url_group('Necessary_work_tabs')
 
 ## It's highly recommended that you write your shortcut scripts here in this same directory and then make shortcut pointer
 ## file types for your desktop/other directories. This is really just to maintain organization and refactorability.
